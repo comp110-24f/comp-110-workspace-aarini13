9b6d4ef37588c23431e86132d451708d82d27fa5
Aarini Chauhan <aarini@unc.edu>
2024-09-24 13:17:51 -0400

    3 times a charm ex02

--- exercises/ex02_chardle.py
+++ exercises/ex02_chardle.py
@@ -6,17 +6,17 @@ __author__ = "730550999"
 
 
 def main() -> None:
-    contains_char()
+    word2: str = input_word()
+    letter2: str = input_letter()
+    contains_char(word=word2, letter=letter2)
 
 
 def input_word() -> str:
-    guess_word = str(input("Enter a 5-character word: "))
+    guess_word: str = str(input("Enter a 5-character word: "))
     if len(guess_word) != 5:  # could also do it reverse way where len == 5
         print("Error: Word must contain 5 characters.")
         exit()
-        return guess_word
     else:
-        print(guess_word)
         return guess_word
 
 
@@ -25,24 +25,25 @@ def input_letter() -> str:
     if len(guess_letter) != 1:
         print("Error: Character must be a single character.")
         exit()
-        return guess_letter
     else:
         return guess_letter
 
 
-def contains_char(word=input_word(), letter=input_letter()) -> None:
+def contains_char(word: str, letter: str) -> None:
     print("Searching for " + letter + " in " + word)
     letter_index: int = 0
-    count: int = 0  # need both in order to keep track of different variables
+    letter_count: int = 0  # need both in order to keep track of different variables
     while letter_index <= 4:  # too complex to put the length of the word inputter
         if letter == word[letter_index]:
+            letter_count += 1
             print(str(letter) + " found at index " + str(letter_index))
             letter_index += 1
-            count += 1
         else:
             letter_index += 1
-    if count >= 1:
-        print(str(count) + " instances of " + letter + " found in " + word)
+    if letter_count == 1:
+        print("1 instance of " + letter + " found in " + word)
+    elif letter_count > 1:
+        print(str(letter_count) + " instances of " + letter + " found in " + word)
     else:
         print(
             "No instances of " + letter + " found in " + word
